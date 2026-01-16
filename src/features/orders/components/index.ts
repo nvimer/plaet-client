84575710b65ee7a0177358707138154eea7da043
Nicolas Pantoja Diaz <nspantojadiaz@gmail.com>
2026-01-16 16:02:54 -0500

    feat(orders): Implement new styles to orders components

--- src/features/orders/components/index.ts
+++ src/features/orders/components/index.ts
@@ -2,4 +2,6 @@ export { OrderStatusBadge } from "./OrderStatusBadge";
 export { OrderTypeBadge } from "./OrderTypeBadge";
 export { OrderCard } from "./OrderCard";
 export { OrderFilters } from "./OrderFilters";
-export * from "./modals";
+export { ProductGrid } from "./ProductGrid";
+// Note: Modals are deprecated - use pages instead
+// export * from "./modals";
